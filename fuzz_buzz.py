d4bb9719c09cf2631ce9f282c4371e5828a94edf
Ian Galbraith <igalbraith@berkeley.edu>
2017-09-30 18:12:15 -0700

    Classic fizzbuzz question

---
+++ fuzz_buzz.py
@@ -0,0 +1,12 @@
+inputed = input("Next prime y/n?")
+prime = 1
+
+for x in range(1, int(inputed)):
+    if x % 3 == 0 and x % 5 == 0:
+        print('FizzBuzz')
+    elif x % 3 == 0:
+        print('Fizz')
+    elif x % 5 == 0:
+        print('Buzz')
+    else:
+        print(x)
